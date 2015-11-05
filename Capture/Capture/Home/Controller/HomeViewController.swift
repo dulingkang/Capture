77745c294911a1246bdc674b3275d57cb3d3bba7
shawnDu <shuaishuai.du@smartereye.com>
2015-11-05 13:45:08 +0800

    create camera, home, adjust, share folder

---
+++ Capture/Capture/Home/Controller/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  Capture
+//
+//  Created by ShawnDu on 15/11/5.
+//  Copyright © 2015年 ShawnDu. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+}
