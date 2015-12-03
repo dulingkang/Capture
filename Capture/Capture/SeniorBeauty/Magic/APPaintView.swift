4cdcd0950294e78571d7da6eb268a3e8653a5c94
dulingkang <dulingkang@163.com>
2015-12-03 22:34:45 +0800

    add PaintView

---
+++ Capture/Capture/SeniorBeauty/Magic/APPaintView.swift
@@ -0,0 +1,13 @@
+//
+//  APPaintView.swift
+//  Capture
+//
+//  Created by dulingkang on 3/12/15.
+//  Copyright © 2015 ShawnDu. All rights reserved.
+//
+
+import UIKit
+
+class APPaintView: UIView {
+    
+}
